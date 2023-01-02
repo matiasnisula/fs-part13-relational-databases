e7f3f9110fa5399b8a374ede378bf90425353fa7
Matias Nisula <matiasnisula81@gmail.com>
2023-01-02 17:34:32 +0200

    13.4

--- index.js
+++ index.js
@@ -1,20 +1,79 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    });
-    console.log("blogs", blogs);
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "blog",
   }
-};
+);
 
-main();
+Blog.sync();
+
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+app.post("/api/blogs", async (req, res) => {
+  console.log("body:", req.body);
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+app.delete("/api/blogs/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log("id:", id);
+  const result = await Blog.destroy({
+    where: {
+      id: id,
+    },
+  });
+  console.log("result:", result);
+  if (result === 1) {
+    return res.status(204).end();
+  }
+  res.status(400).json({ error: `Cant find blog with id ${id}` });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
