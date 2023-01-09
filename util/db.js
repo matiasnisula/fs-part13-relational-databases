6b1ca88969f848d9cff54955cbd3c5e051a18de2
Matias Nisula <matiasnisula81@gmail.com>
2023-01-09 14:00:37 +0200

    13.17

--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const { DATABASE_URL } = require("./config");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialect: "postgres",
@@ -10,17 +11,42 @@ const sequelize = new Sequelize(DATABASE_URL, {
   timezone: "+02:00",
 });
 
+const migrationConf = {
+  migrations: {
+    glob: "migrations/*.js",
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
-    //await sequelize.query("SET timezone = '+02:00';", { raw: true });
+    await runMigrations();
+    //await sequelize.query("SET timezone = 'Europe/Helsinki';", { raw: true });
     console.log("connected to the database");
   } catch (err) {
-    console.log(err);
     console.log("failed to connect to the database");
+    console.log(err);
     return process.exit(1);
   }
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+module.exports = { connectToDatabase, sequelize, rollbackMigration };
